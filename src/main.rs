1dbb83581e252c4f58ccde797a0b0a2bb0a4e08b
mirumi <info@milmemo.net>
2022-05-19 09:39:17 +0900

    [add] : rust-5

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,3 @@
 fn main() {
-    let mut s = "heelo";
-    s = "hey";
-    println!("Hello, world! {}", s);
-
-    let mut s = String::from("hello");
-    println!("! {}", s);
-
-
-    s.push_str(", world");
-    println!("! {}", s);
-
-
-    let s = String::from("hello");  // sがスコープに入る
-
-    takes_ownership(s);             // sの値が関数にムーブされ...
-                                    // ... ここではもう有効ではない
-
-    let x = 5;                      // xがスコープに入る
-
-    makes_copy(x);  
-
-            println!("{}", x);
-
+    
 }
-
-fn takes_ownership(some_string: String) { // some_stringがスコープに入る。
-    println!("{}", some_string);
-} // ここでsome_stringがスコープを抜け、`drop`が呼ばれる。後ろ盾してたメモリが解放される。
-  // 
-
-fn makes_copy(some_integer: i32) { // some_integerがスコープに入る
-    println!("{}", some_integer);
-} // ここでsome_integerがスコープを抜ける。何も特別なことはない。
--- src/rust-5.rs
+++ src/rust-5.rs
@@ -1,9 +1,59 @@
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
+    let width1 = 30;
+    let height1 = 50;
+
+    let rectangle = Rectangle {
+        width: width1,
+        height: height1,
+    };
+    println!("{:#?}", rectangle);
+
+    println!("{:#?}", rectangle.area());
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("{:#?}", Rectangle::square(3));
+
+
+
+    println!("The area of the rectangle is {} square pixels.", area(&rectangle));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
     
 
 
 
 
 
-}
 
